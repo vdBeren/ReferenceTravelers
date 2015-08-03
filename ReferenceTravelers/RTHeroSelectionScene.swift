a96e65170374550c8f55f862f6c8c1ea3c6a6ae2
vdBeren <vdias.savariego@gmail.com>
2015-08-03 04:44:48 -0300

    SelectionScene

---
+++ ReferenceTravelers/RTHeroSelectionScene.swift
@@ -0,0 +1,160 @@
+//
+//  RTHeroSelectionScene.swift
+//  ReferenceTravelers
+//
+//  Created by Victor D. Savariego on 13/7/15.
+//  Copyright (c) 2015 RTTeam. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+
+class RTHeroSelectionScene: SKScene {
+    
+    var backgroundNode: RTBackground?
+    var selectionMenu: RTSelectionMenu?
+    var textWindow: RTHeroTextWindow?
+    
+    override init(size: CGSize) {
+        
+        super.init(size: size)
+        
+        
+        //Background da Scene
+        backgroundNode = RTBackground(imageNamed: "bgHeroSelection")
+        backgroundNode!.position = CGPoint(x: 0.0, y: 0.0)
+        self.addChild(backgroundNode!)
+        
+        // Text Window
+        textWindow = RTHeroTextWindow()
+        self.addChild(textWindow!)
+        
+        // Selection Menu
+        let arrayImageNames = ["btnSelectionLeft", "btnSelectionRight", "btnText", "barText"]
+        let arrayText = ["HIRE", "CHOOSE"]
+        let colors = [SKColor.orangeColor(), SKColor.whiteColor()]
+        
+        selectionMenu = RTSelectionMenu(objectArray: GHeroesManager!.heroArray, buttonsImageNames: arrayImageNames, selectButtonTexts: arrayText, colors: colors)
+        self.buildSelectionMenu()
+        self.addChild(selectionMenu!)
+        
+        self.userInteractionEnabled = false
+
+    }
+    
+    func restoreScene(){
+        
+        let alpha = SKAction.fadeAlphaTo(1.0, duration: 0.8)
+        
+        let blockHidden = SKAction.runBlock({
+            self.textWindow?.runAction(alpha)
+            self.selectionMenu?.runAction(alpha)
+            self.selectionMenu?.userInteractionEnabled = true
+        })
+        
+        self.runAction(blockHidden)
+        
+    }
+    
+    func returningFromStage(){
+        
+        let zoom = SKAction.scaleTo(1.0, duration: 1.0)
+        let move = SKAction.moveByX(880, y: 630, duration: 1.0)
+        let group = SKAction.group([zoom, move])
+        
+        let block = SKAction.runBlock({self.restoreScene()})
+        let sequence = SKAction.sequence([group, block])
+        
+        self.backgroundNode!.runAction(sequence)
+    }
+    
+    private func buildSelectionMenu(){
+        
+        
+        selectionMenu?.setRTChangeSelectionAction({ () -> () in
+            let selectionIndex: Int = self.selectionMenu!.selectionIndex
+            let selectionHero: RTSelectable = self.selectionMenu!.objectArray[selectionIndex]
+            
+            GHeroesManager!.currentHero = (selectionHero as? RTHero)!
+            self.textWindow!.refreshContents()
+            
+        })
+        
+        selectionMenu?.setRTSelectButton({ () -> () in
+            
+            let alpha = SKAction.fadeAlphaTo(0.0, duration: 0.4)
+            
+            self.selectionMenu?.userInteractionEnabled = false
+
+            
+            let blockHidden = SKAction.runBlock({
+                self.textWindow?.runAction(alpha)
+                self.selectionMenu?.runAction(alpha)
+            })
+            
+            let wait = SKAction.waitForDuration(0.8)
+            
+            let sequenceHidden = SKAction.sequence([blockHidden, wait])
+            
+            self.runAction(sequenceHidden)
+            
+            
+            let zoom = SKAction.scaleTo(2.5, duration: 1.0)
+            let move = SKAction.moveByX(-880, y: -630, duration: 1.0)
+            let group = SKAction.group([zoom, move])
+            
+            let block = SKAction.runBlock({
+                
+                let transition = SKTransition.pushWithDirection(SKTransitionDirection.Up, duration: 1.5)
+                self.scene?.view?.presentScene(GStageSelectionScene, transition: transition)
+            })
+            
+            let sequence = SKAction.sequence([group, block])
+            
+            self.backgroundNode!.runAction(sequence)
+
+        })
+        
+        var sizeW = self.size.width
+        var sizeH = self.size.height
+        
+        selectionMenu!.btnLeft?.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        selectionMenu!.btnLeft!.position = CGPoint(x: sizeW/3.1, y: sizeH/1.7)
+        
+        selectionMenu!.btnRight?.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        selectionMenu!.btnRight!.position = CGPoint(x: sizeW/1.45, y: sizeH/1.7)
+        
+        selectionMenu!.btnSelect?.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        selectionMenu!.btnSelect!.position = CGPoint(x: sizeW/2, y: sizeH/2.6)
+        
+        selectionMenu!.selectableDisplay?.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        selectionMenu!.selectableDisplay?.position = CGPoint(x: sizeW/2, y: sizeH/1.75)
+        
+        
+        selectionMenu!.labelObjectName?.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        selectionMenu!.labelObjectName?.position = CGPoint(x: sizeW/2, y: sizeH/1.3)
+        
+        
+    }
+    
+    //Chamado a todo frame. Usado para realizar o update dos nodes da Scene, em cascata.
+    override func update(currentTime: NSTimeInterval) {
+        super.update(currentTime)
+        
+    }
+    
+    
+    //Recebe toques na Scene.
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        
+        
+    }
+    
+    
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ ReferenceTravelers/RTStageSelectionScene.swift
@@ -0,0 +1,132 @@
+//
+//  RTStageSelectionScene.swift
+//  ReferenceTravelers
+//
+//  Created by Victor D. Savariego on 13/7/15.
+//  Copyright (c) 2015 RTTeam. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+
+class RTStageSelectionScene: SKScene {
+    
+    var btnBack: RTBoingButton?
+    var backgroundNode: RTBackground?
+    var selectionMenu: RTSelectionMenu?
+    var textWindow: RTStageTextWindow?
+    
+    override init(size: CGSize) {
+        
+        super.init(size: size)
+        
+        
+        //Background da Scene
+        backgroundNode = RTBackground(imageNamed: "bgStageSelection")
+        backgroundNode!.position = CGPoint(x: 0.0, y: 0.0)
+        self.addChild(backgroundNode!)
+
+        
+        // Selection Menu
+        
+        let arrayImageNames = ["btnSelectionLeft", "btnSelectionRight", "hologramaLuz", "barText"]
+        let arrayText = ["RESEARCH", "TRAVEL!"]
+        let colors = [SKColor.whiteColor(), SKColor.clearColor()]
+        
+        selectionMenu = RTSelectionMenu(objectArray: GStageManager!.stageArray, buttonsImageNames: arrayImageNames, selectButtonTexts: arrayText, colors: colors)
+        self.buildSelectionMenu()
+        self.addChild(selectionMenu!)
+        
+        // Text Window
+        textWindow = RTStageTextWindow()
+        textWindow?.zPosition += 1
+        self.addChild(textWindow!)
+        
+        btnBack = RTBoingButton(imageNamed: "btnBack", actionOnTouchBegan: true)
+        self.btnBack!.position = CGPoint(x: self.size.width/30, y: self.size.height/1.1)
+        self.btnBack!.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        btnBack?.zPosition += 1
+        
+        //BLOCO DE AÇÃO DO BOTÃO
+        btnBack?.setRTButtonAction({ () -> () in
+            let block = SKAction.runBlock({
+                GHeroSelectionScene!.returningFromStage()
+                let transition = SKTransition.pushWithDirection(SKTransitionDirection.Down, duration: 1.5)
+                self.scene?.view?.presentScene(GHeroSelectionScene, transition: transition)
+            })
+            
+            let wait = SKAction.waitForDuration(0.3)
+            let sequence = SKAction.sequence([wait, block])
+            
+            self.runAction(sequence)
+
+        })
+        self.addChild(btnBack!)
+        
+        
+    }
+    
+    private func buildSelectionMenu(){
+        
+        selectionMenu?.setRTChangeSelectionAction({ () -> () in
+            let selectionIndex: Int = self.selectionMenu!.selectionIndex
+            let selectionStage: RTSelectable = self.selectionMenu!.objectArray[selectionIndex]
+            
+            GStageManager!.currentStage = (selectionStage as? RTStage)!
+            self.textWindow!.refreshContents()
+            
+        })
+        
+        selectionMenu?.setRTSelectButton({ () -> () in
+            self.runAction(SKAction.waitForDuration(0.2))
+            let transition = SKTransition.pushWithDirection(SKTransitionDirection.Down, duration: 1.5)
+            self.scene?.view?.presentScene(GBoardScene, transition: transition)
+        })
+        
+        var sizeW = self.size.width
+        var sizeH = self.size.height
+        
+        selectionMenu!.btnLeft?.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        selectionMenu!.btnLeft!.position = CGPoint(x: sizeW/6.5, y: sizeH/1.5)
+        
+        selectionMenu!.btnRight?.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        selectionMenu!.btnRight!.position = CGPoint(x: sizeW/1.61, y: sizeH/1.5)
+        
+        selectionMenu!.btnSelect?.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        selectionMenu!.btnSelect?.position = CGPoint(x: sizeW/1.18, y: sizeH/1.7)
+        selectionMenu!.btnSelect?.labelText?.fontSize = 45.0
+        selectionMenu!.btnSelect?.labelText?.position.y += 40
+        
+        let hologramPad = SKSpriteNode(imageNamed: "hologramaPad")
+        hologramPad.position.y -= 300
+        selectionMenu!.btnSelect?.addChild(hologramPad)
+        
+        selectionMenu!.selectableDisplay?.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        selectionMenu!.selectableDisplay?.position = CGPoint(x: sizeW/2.613, y: sizeH/1.507)
+        
+        selectionMenu!.labelObjectName?.hidden = true
+        selectionMenu!.labelObjectName?.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        selectionMenu!.labelObjectName?.position = CGPoint(x: sizeW/2, y: sizeH/1.3)
+
+    }
+    
+    //Chamado a todo frame. Usado para realizar o update dos nodes da Scene, em cascata.
+    override func update(currentTime: NSTimeInterval) {
+        super.update(currentTime)
+        
+    }
+    
+    
+    //Recebe toques na Scene.
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+
+    }
+    
+    
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
